915152e864fb7e6ab6e547f037f64ed29283db54
seamus tuohy <s2e@seamustuohy.com>
2016-02-17 11:04:50 -0500

    Added new interface name identification regex functions.

--- create_ap/config.py
+++ create_ap/config.py
@@ -43,19 +43,61 @@ class ConfigWriter(Config):
 
     def add_rule(self, ap_name="copilot", ap_password="copilot_pass", iface_in="eth0", iface_out=None):
         if not iface_out:
-            try:
-                iface_out = self.get_proc_wifi_interface()
-            except RuntimeError:
-                iface_out = self.get_sys_wifi_interface()
+            iface_out = get_wireless_interface()
         log.debug("adding create ap rule  {0} {1} {2} {3}".format(iface_out, iface_in, ap_name, ap_password))
         self._rules.append("{0} ".format(iface_out))
         self._rules.append("{0} ".format(iface_in))
         self._rules.append("{0} ".format(ap_name))
         self._rules.append("{0} ".format(ap_password))
 
-    def get_proc_wifi_interface(self):
+    def get_wireless_interface():
+        iface_out = None
+        name_regex = get_interface_regex("wlan")
+        try:
+            iface_out = self.get_proc_wifi_interface(name_regex)
+        except RuntimeError:
+            iface_out = self.get_sys_wifi_interface(name_regex)
+        if iface_out = None:
+            raise RuntimeError("Unable to identify wireless interface.")
+        else:
+            return iface_out
+
+    def get_interface_regex(interface_type):
+        """ Get a interface regex
+
+
+        Per: https://github.com/systemd/systemd/blob/5031c4e21b4379c1824033be675517978090fd36/src/udev/udev-builtin-net_id.c#L20
+
+        Regex currently only supports the following types of names:
+            [BCMA, CCW, on_board, hotplug, MAC]
+        NOTE: Naming by PCI geographical location and USB port number chain not supported.
+
+        Args:
+            interface_type (string):
+                acceptable values = [eth, serial, wlan, wwan]
+
+        """
+
+        interface_prefixes = {"eth":"en", "serial":"sl",
+                              "wlan":"wl", "wwan":"ww"}
+        interface_types = [
+            "b", # BCMA
+            "c", # CCW
+            "o", # on_board
+            "s", # hotplug
+            "x"] # MAC
+        name_components = {}
+        try:
+            name_components['prefix'] = interface_prefixes['interface_type']
+        except KeyError:
+            raise ValueError("{0} is an invalid interface prefix.".format(interface_type) +
+                             "Please use one of the following: {0}".format(interface_prefixes))
+        name_components['types'] = "".join(interface_types)
+        interface_name_regex = re.compile("{prefix}[{types}]\S*".format(**name_components))
+        return interface_name_regex
+
+    def get_proc_wifi_interface(self, wifi_device_regex):
         log.debug("Searching for wireless interface in /proc/net/wireless.")
-        wifi_device_regex = re.compile('w[a-z]{1,3}[0-9]')
         ifnames = []
         with open('/proc/net/wireless', 'r') as wireless_process_info:
             file_dump = wireless_process_info.readlines()
@@ -72,9 +114,8 @@ class ConfigWriter(Config):
         # We may have to change this once we start allowing wireless to wireless bridging
         return ifnames[0]
 
-    def get_sys_wifi_interface(self):
+    def get_sys_wifi_interface(self, wifi_device_regex):
         log.debug("Searching for wireless interface in /sys/class/net.")
-        wifi_device_regex = re.compile('w[a-z]{1,3}[0-9]')
         wifi_ifaces = []
         net_iface_root = "/sys/class/net"
         iface_directories = os.listdir(net_iface_root)
